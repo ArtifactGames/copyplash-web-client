16e2201aeeb0468dfbbc9f3b40e8f0b88924c39d
Adoankim <adoankim@gmail.com>
2018-03-19 23:07:09 +0100

    Adds Communication component to handle websocket layer

--- js/main.js
+++ js/main.js
@@ -3,13 +3,72 @@ const Env = {
     serverPort: "80"
 }
 
-class Communication {
-    constructor(){
-        this.ws = WebSocket(`ws://${Env.serverAddress}:${Env.serverPort}`)
+class Communication extends React.PureComponent {
+    constructor(props){
+        super(props);
+        this.webSocket = null;
+        this.state = { error: null, message: null, isOpen: false}
+
+        this.connect = this.connect.bind(this);
+        this.onOpen = this.onOpen.bind(this);
+        this.onClose = this.onClose.bind(this);
+        this.onMessage = this.onMessage.bind(this);
+        this.onError = this.onError.bind(this);
+    }
+
+    connect (lobbyId) {
+        if (this.webSocket == null || !this.state.isOpen) {
+            this.webSocket = new WebSocket(`ws://${Env.serverAddress}:${Env.serverPort}/${lobbyId}`)
+            this.webSocket.onopen = this.onOpen
+            this.webSocket.onclose = this.onClose
+            this.webSocket.onmessage = this.onMessage
+            this.webSocket.onerror = this.onError
+        }
+    }
+
+    disconnect () {
+        if (this.webSocket != null && this.state.isOpen) {
+            this.webSocket.close();
+        }
+    }
+
+    onOpen (event) {
+        this.setState({ isOpen: true })
+    }
+
+    onClose (event) {
+        this.setState({ isOpen: false })
+    }
+
+    onError (error) {
+        this.setState({ error })
+    }
+
+    onMessage (message) {
+        this.setState({ message })
+    }
+
+    render () {
+        const { children } = this.props;
+
+        const childrenState = { 
+            message: this.state.message, 
+            error: this.state.error, 
+            connect: this.connect, 
+            disconnect: this.disconnect 
+        }
+
+        const childrenWithProps = React.Children.map(children, child =>
+            React.cloneElement(child, childrenState));
+
+        return (
+            <div>{childrenWithProps}</div>
+        )
     }
 }
 
-class JoinForm extends React.Component {
+
+class JoinForm extends React.PureComponent {
     constructor(props) {
         super(props);
         this.state = { nick: '', password: '' };
@@ -63,15 +122,26 @@ function Greeting() {
     </p>
 }
 
-function JoinScreen() {
+function JoinScreen(props) {
     return <div>
         <Header />
         <Greeting />
-        <JoinForm />
+        <JoinForm {...props}/>
     </div>
 }
 
+function getCurrentScreen() {
+    //TODO navigate to other screen when some condition changed
+    return <JoinScreen />
+}
+
+const App = () => (
+    <Communication>
+        { getCurrentScreen() }
+    </Communication>
+)
+
 ReactDOM.render(
-    <JoinScreen />,
+    <App />,
     document.getElementById('root')
 );
