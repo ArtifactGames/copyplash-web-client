4bb592b2c838a04d71df53ca779583e4db2be157
Adoankim <adoankim@gmail.com>
2018-03-20 22:58:24 +0100

    Adds application/json headers to the login request

--- src/JoinForm.js
+++ src/JoinForm.js
@@ -23,6 +23,10 @@ export class JoinForm extends React.PureComponent {
     async doLogin() {
         try {
             const options = {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
                 method: "POST",
                 body: JSON.stringify({ password: this.state.password })
             }
